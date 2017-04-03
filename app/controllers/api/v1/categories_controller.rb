0ed6ddc15ef22f77d2439991b5030ac7ffb39d61
Ruben <ssrcruz@gmail.com>
2017-04-03 16:02:06 -0400

    Setup a basic API

---
+++ app/controllers/api/v1/categories_controller.rb
@@ -0,0 +1,25 @@
+class Api::V1::CategoriesController < Api::V1::BaseController
+  def index
+    respond_with Category.all
+  end
+
+  def create
+    respond_with :api, :v1, Category.create(category_params)
+  end
+
+  def destroy
+    respond_with Category.destroy(params[:id])
+  end
+
+  def update
+    category = Category.find(params["id"])
+    category.update_attributes(category_params)
+    respond_with category, json: category
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:title, :description)
+  end
+end
