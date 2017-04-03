5280fe13bb81a8f504c1a2e28ee785c9ff6fdd7b
Ruben <ssrcruz@gmail.com>
2017-04-02 21:39:56 -0400

    Add bootstrap

--- test/system/welcome_controllers_test.rb
+++ test/system/welcome_controllers_test.rb
@@ -3,7 +3,7 @@ require "application_system_test_case"
 class WelcomeControllersTest < ApplicationSystemTestCase
   test "visiting the index" do
     visit root_url
-    accept_alert('Webpacker')
+
     assert_selector "h1", text: "WelcomeController"
   end
 end
