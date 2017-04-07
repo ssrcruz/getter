e34146f2f93ea1a3e36e2b49398373e624c1b2e0
Ruben <ssrcruz@gmail.com>
2017-04-06 22:16:54 -0400

    make a change

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
   root to: 'site#index'
 
+  # get '/blogs', to: 'site#'
   get '/categories/:id', to: 'site#show', as: 'categories'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
