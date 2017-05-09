dbb0cc5a7424133743d93140b35d09f142269be3
Ruben <ssrcruz@gmail.com>
2017-05-09 17:37:04 -0400

    change something

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -3,9 +3,10 @@ import 'babel-polyfill'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import store from '../store/configureStore'
-import AppComponent from '../components/App'
 import { Provider } from 'react-redux'
 import { connect } from 'react-redux'
+import AppComponent from '../components/App'
+
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
